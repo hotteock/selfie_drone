f7d9930219ee40df3d8ee07649ec0af46ce50896
hotteock <38280229+hotteock@users.noreply.github.com>
2020-01-01 14:25:51 +0900

    1/1
    1/1_ver1

--- opencv_dnn_ver2.py
+++ opencv_dnn_ver2.py
@@ -1,3 +1,7 @@
+from imutils.video import WebcamVideoStream
+from imutils.video import FPS
+import argparse
+import imutils
 import cv2, time
 
 # load model
@@ -7,25 +11,28 @@ net = cv2.dnn.readNetFromTensorflow(model_path, config_path)
 
 conf_threshold = 0.7
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+	help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+	help="Whether or not frames should be displayed")
+args = vars(ap.parse_args())
+
 # initialize video source, default 0 (webcam)
 video_path = 'videos/RollerCoasters.mp4'
-cap = cv2.VideoCapture(0)
+cap = WebcamVideoStream(src=0).start()
 
 frame_count, tt = 0, 0
+fps = FPS().start()
 
-while cap.isOpened():
-  ret, img = cap.read()
-  if not ret:
-    break
-
-  frame_count += 1
-
-  start_time = time.time()
+while fps._numFrames < args["num_frames"]:
+  img = cap.read()
+  img = imutils.resize(img, width=300)
 
   # prepare input
   result_img = img.copy()
   h, w, _ = result_img.shape
-  blob = cv2.dnn.blobFromImage(result_img, 1.0, (200, 200), [104, 117, 123], False, False)
+  blob = cv2.dnn.blobFromImage(result_img, 1.0, (300, 300), [104, 117, 123], False, False)
   net.setInput(blob)
 
   # inference, find faces
@@ -45,15 +52,14 @@ while cap.isOpened():
       cv2.putText(result_img, '%.2f%%' % (confidence * 100.), (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
   # inference time
-  tt += time.time() - start_time
-  fps = frame_count / tt
+  fps.update()
   cv2.putText(result_img, 'FPS(dnn): %.2f' % (fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
   # visualize
-  cv2.imshow('result', result_img)
-  if cv2.waitKey(1) == ord('q'):
-    break
+  if args["display"] > 0:
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
 
-
+fps.stop()
 cap.release()
 cv2.destroyAllWindows()
